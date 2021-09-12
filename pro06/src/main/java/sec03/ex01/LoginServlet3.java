bfe681c647f502f01606bb026eaa508b2d189050
dyless0811 <dyless0811@gmail.com>
2021-09-12 20:37:08 +0900

    Migrating tomcat10 => tomcat9

--- pro06/src/main/java/sec03/ex01/LoginServlet3.java
+++ pro06/src/main/java/sec03/ex01/LoginServlet3.java
@@ -1,16 +1,16 @@
 package sec03.ex01;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import jakarta.servlet.Servlet;
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebServlet;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 /**
  * Servlet implementation class LoginServlet3
